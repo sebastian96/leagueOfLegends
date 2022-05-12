34a62fd8c175ca32e5dbfbd5f69a35c36838b55e
sebastian miranda <arqcompu4@gmail.com>
2022-05-11 23:11:33 -0500

    config build and github pages

--- src/utils/petitions.js
+++ src/utils/petitions.js
@@ -1,4 +1,4 @@
-const apiConsumption = async (endpoint) => {
+export const apiConsumption = async (endpoint) => {
     const lastVersion = await getLastVersion();
     const baseURL = `https://ddragon.leagueoflegends.com/cdn/${lastVersion}/data/es_MX/`;
     const data = await fetch(`${baseURL}${endpoint}`);
@@ -6,14 +6,9 @@ const apiConsumption = async (endpoint) => {
     return data.json();
 }
 
-const getLastVersion = async () => {
+export const getLastVersion = async () => {
     const baseURL = "https://ddragon.leagueoflegends.com/api/versions.json";
     const data = await fetch(baseURL);
 
     return data.json().then(versions =>  versions[0])
-}
-
-module.exports = {
-    apiConsumption,
-    getLastVersion
 }
